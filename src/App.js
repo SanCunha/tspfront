2911c89747d3d28dbc6c4fee57bb0a8730141aa9
San Cunha <san.cunha@upflow.me>
2022-07-17 20:28:10 -0300

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+import Params from "./components/params";
+import Logs from "./components/logs";
+import Solution from "./components/solution";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [logs, setLogs] = useState([]);
+  const [solution, setSolution] = useState({
+    route: "-",
+    value: "-",
+    coords: [
+      [93, 15, 38, 65, 49, 56, 59, 35, 40, 42],
+      [76, 40, 8, 38, 7, 72, 85, 82, 96, 12],
+    ],
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Top-Painel">
+        <div className="Left-Display">
+          <Params setLogs={setLogs} setSolution={setSolution} />
+        </div>
+        <div className="Right-Display">
+          <Solution data={solution} />
+        </div>
+      </div>
+      <div className="Botton-Painel">
+        <Logs logs={logs} />
+      </div>
     </div>
   );
 }
